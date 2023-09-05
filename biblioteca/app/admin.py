fc4c3c6cfdcfc7eaaf0f78c30906db79734ceda0
Luisz576 <56451779+Luisz576@users.noreply.github.com>
2023-09-04 22:24:55 -0300

    projeto

---
+++ biblioteca/app/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from app.models import *
+
+admin.site.register(Cidade)
+admin.site.register(Autor)
+admin.site.register(Editora)
+admin.site.register(Categoria)
+admin.site.register(Livro)
+admin.site.register(Leitor)
+admin.site.register(Emprestimo)
---
+++ biblioteca/app/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+
+
+class Cidade(models.Model):
+    nome = models.CharField(max_length=45)
+    uf = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.nome
+
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=45)
+    site = models.CharField(max_length=80)
+    cidade = models.ForeignKey(Cidade, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=45)
+    site = models.CharField(max_length=80)
+    cidade = models.ForeignKey(Cidade, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.nome
+
+
+class Livro(models.Model):
+    nome = models.CharField(max_length=45)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    editora = models.ForeignKey(Editora, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    preco = models.FloatField()
+    data_publicacao = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.nome} - {self.autor}'
+
+
+class Leitor(models.Model):
+    nome = models.CharField(max_length=45)
+    email = models.CharField(max_length=60)
+    cpf = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nome
+
+
+class Emprestimo(models.Model):
+    data_emprestimo = models.DateTimeField()
+    livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
+    leitor = models.ForeignKey(Leitor, on_delete=models.CASCADE)
+    data_devolucao = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.livro} - {self.leitor} - {self.data_emprestimo} - {self.data_devolucao}'
